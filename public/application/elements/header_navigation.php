922f58ebb290f7fa4293c5575391701fb3d13a1a
Fabian Bitter <fabian@bitter.de>
2021-03-12 16:34:53 +0100

    Fix header nav

--- public/application/elements/header_navigation.php
+++ public/application/elements/header_navigation.php
@@ -1,36 +1,80 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php
 
-<header class="navbar navbar-static-top documentation-header" role="banner">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="navbar-header">
-                    <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".documentation-navbar-collapse">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <a href="<?=DIR_REL?>/" class="header-logo"><img src="<?=$view->getThemePath()?>/images/logo.png" style="width: 160px">
-                    <span><?=t('translate')?></span></a>
-                </div>
-                <nav class="collapse navbar-collapse documentation-navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active">
-                            <a href="<?=DIR_REL?>/teams">Translation teams</a>
-                        </li>
-                        <li>
-                            <a href="<?=DIR_REL?>/translate">Translate</a>
-                        </li>
-                        <li>
-                            <a href="<?=DIR_REL?>/translate-your-packages">Translate your packages</a>
-                        </li>
-                        <li class="hidden-xs">
-                            <div class="wcbit-toggle">
+/**
+ * @project:   ConcreteCMS Theme
+ *
+ * @copyright  (C) 2021 Portland Labs (https://www.portlandlabs.com)
+ * @author     Fabian Bitter (fabian@bitter.de)
+ */
 
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-    <?php View::element('header_notifications', array(), 'concrete5_theme'); ?>
-</header>
+defined('C5_EXECUTE') or die('Access Denied.');
+
+use Concrete\Core\Support\Facade\Url;
+
+use Concrete\Core\Config\Repository\Repository;
+use Concrete\Core\Page\Page;
+use Concrete\Core\Support\Facade\Application;
+use Concrete\Core\User\User;
+use Concrete\Core\Validation\CSRF\Token;
+
+$app = Application::getFacadeApplication();
+/** @var Token $token */
+$token = $app->make(Token::class);
+$site = $app->make('site')->getActiveSiteForEditing();
+/** @var Repository $siteConfig */
+$siteConfig = $site->getConfigRepository();
+
+?>
+
+<ul class="nav navbar-nav navbar-right">
+    <li class="nav-item index-1">
+        <a href="<?php echo (string)Url::to("/teams"); ?>" target="_self" class="nav-link">
+            <?php echo t("Translation Teams"); ?>
+        </a>
+    </li>
+
+    <li class="nav-item index-2">
+        <a href="<?php echo (string)Url::to("/translate"); ?>" target="_self" class="nav-link">
+            <?php echo t("Translate"); ?>
+        </a>
+    </li>
+
+    <li class="nav-item index-3">
+        <a href="<?php echo (string)Url::to("/translate-your-packages"); ?>" target="_self" class="nav-link">
+            <?php echo t("Translate your packages"); ?>
+        </a>
+    </li>
+
+    <?php
+
+    // add search icon
+    $searchPageId = (int)$siteConfig->get("concrete_cms_theme.search_page_id");
+    $searchPage = Page::getByID($searchPageId);
+
+    if ($searchPage instanceof Page && !$searchPage->isError()) {
+        echo '<li class="d-none d-lg-block nav-item">';
+        echo '<a href="' . (string)Url::to($searchPage) . '" title="' . h(t("Search")) . '" class="nav-link"><i class="fas fa-search"></i></a>';
+        echo '</li>';
+    }
+
+    // add user icon
+
+    $user = new User();
+
+    if ($user->isRegistered()) {
+        echo '<li class="d-none d-lg-block nav-item">';
+        echo '<a href="' . (string)Url::to('/login', 'do_logout', $token->generate('do_logout')) . '" title="' . h(t("Sign Out")) . '" class="nav-link"><i class="fas fa-sign-out-alt"></i></a>';
+        echo '</li>';
+        echo '<li class="d-block d-lg-none nav-item">';
+        echo '<a href="' . (string)Url::to('/login', 'do_logout', $token->generate('do_logout')) . '" title="' . h(t("Sign Out")) . '" class="nav-link">' . t("Sign Out") . '</a>';
+        echo '</li>';
+    } else {
+        echo '<li class="d-none d-lg-block nav-item">';
+        echo '<a href="' . (string)Url::to('/login') . '" title="' . h(t("Sign In")) . '" class="nav-link"><i class="fas fa-sign-in-alt"></i></a>';
+        echo '</li>';
+        echo '<li class="d-block d-lg-none nav-item">';
+        echo '<a href="' . (string)Url::to('/login') . '" title="' . h(t("Sign In")) . '" class="nav-link">' . t("Sign In") . '</a>';
+        echo '</li>';
+    }
+    ?>
+</ul>
