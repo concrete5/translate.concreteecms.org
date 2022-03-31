e707b66855d7aeb5c0dcb09f378e7f43277345d7
Korvin Szanto <me@kor.vin>
2022-03-31 11:42:23 -0700

    Revert "Decouple community_translation and theme"
    This reverts commit ced177dd86865251208d33e39449e5f42cb06df3.

--- public/application/elements/footer_navigation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use ConcreteCMS\Translate\Navigation\FooterContentProvider;
-
-defined('C5_EXECUTE') or die('Access Denied.');
-
-echo app(FooterContentProvider::class)->getFooterContent();
