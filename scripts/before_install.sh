c329629677cc49be9ec020b2c71f66cd01b41e98
Korvin Szanto <Korvinszanto@gmail.com>
2023-04-05 14:41:31 -0700

    Use deploy manager for deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-tempdir=/tmp/translate/codedeployupload
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+source ../.codedeployvars
+tempdir=/tmp/codedeploy/codedeployupload/$jobid
 
 [[ -d $tempdir ]] && rm -r $tempdir
 mkdir -p $tempdir
@@ -10,12 +13,3 @@ mkdir /tmp/$DEPLOYMENT_ID
 
 echo "export tempdir=\"$tempdir\"" > "/tmp/$DEPLOYMENT_ID/.cdvariables";
 
-if [ "$APPLICATION_NAME" == "translate.stage.concretecms.org" ]
-then
-  echo "export projectdir=\"/home/forge/translate.stage.concretecms.org\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-  echo "export deploydir=\"/home/forge/translate.stage.concretecms.org/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-elif [ "$APPLICATION_NAME" == "translate.concretecms.org" ]
-then
-  echo "export projectdir=\"/home/forge/translate.concretecms.org\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-  echo "export deploydir=\"/home/forge/translate.concretecms.org/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-fi
