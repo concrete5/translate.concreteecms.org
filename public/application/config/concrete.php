52a34e0e200a1465143483eafcc06aebd0c962cb
Korvin Szanto <korvin.szanto@concretecms.com>
2024-08-30 16:58:04 +0000

    Add concrete auth package

--- public/application/config/concrete.php
+++ public/application/config/concrete.php
@@ -3,5 +3,12 @@
 return [
     'seo' => [
         'url_rewriting_all' => true
-    ]
+    ],
+    'session' => [
+        'max_lifetime' => (new DateTime('+2 days'))->getTimestamp() - time(), // Expire if not accessed for 2 full days
+        'cookie' => [
+            'cookie_secure' => true,
+            'cookie_samesite' => 'None',
+        ],
+    ],
 ];
